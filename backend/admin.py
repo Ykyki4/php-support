ad9d795234e98274603dc95f71ef9cb87b89e96c
Ykyki4 <jimmypantalones@gmail.com>
2023-02-22 12:59:39 +0300

    add user and subscription model

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        if obj:
+            if obj.type == 'employer':
+                return 'telegram_id', 'name', 'type', 'subscription',
+            else:
+                return 'telegram_id', 'name', 'type',
+        else:
+            return 'telegram_id', 'name', 'type',
+
+
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+USER_TYPE_CHOICES = [
+    ('employer', 'Заказчик'),
+    ('freelancer', 'Фрилансер')
+]
+
+
+class Subscription(models.Model):
+    title = models.CharField('Название', max_length=50)
+    price = models.PositiveSmallIntegerField('Цена')
+    max_month_requests = models.PositiveSmallIntegerField('Максимум заявок в месяц')
+    max_response_time = models.PositiveSmallIntegerField('Время рассмотрения заявки(в часах)')
+    extra = models.CharField('Дополнительные возможности', max_length=350)
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    telegram_id = models.CharField('Телеграм идентификатор', max_length=50)
+    name = models.CharField('Имя', max_length=50, null=True)
+    type = models.CharField('Тип пользователя', choices=USER_TYPE_CHOICES, max_length=20)
+    subscription = models.ForeignKey(
+        Subscription,
+        on_delete=models.CASCADE,
+        verbose_name='Подписка',
+        related_name='users',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.name} - {self.get_type_display()}"
