48454df84d8635ff2e6938d3a592c28b4895835a
Ykyki4 <jimmypantalones@gmail.com>
2023-02-24 00:01:18 +0300

    fix useradmin

--- backend/admin.py
+++ backend/admin.py
@@ -15,11 +15,4 @@ class TariffAdmin(admin.ModelAdmin):
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    def get_fields(self, request, obj=None):
-        if obj:
-            if obj.type == 'employer':
-                return 'telegram_id', 'name', 'type', 'subscription',
-            else:
-                return 'telegram_id', 'name', 'type',
-        else:
-            return 'telegram_id', 'name', 'type',
+    pass
